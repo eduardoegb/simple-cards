1b2fee36083f27d7cb845279c322276246ac4ee9
Eduardo Gonzalez Briceno <eduardoegb@gmail.com>
2022-12-20 12:59:21 -0700

    Cards initial version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'simple-cards';
+  posts = [
+    {
+      title: 'Neat Tree',
+      content: 'I saw this awesome tree during my hike today',
+      imageUrl: 'assets/tree.jpeg',
+      username: 'nature',
+    },
+    {
+      title: 'Snowy Mountaint',
+      content: 'I saw this awesome tree during my hike today',
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'mountainlover',
+    },
+    {
+      title: 'Mountain Biking',
+      content: 'I saw this awesome tree during my hike today',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'biking1222',
+    },
+  ];
 }
